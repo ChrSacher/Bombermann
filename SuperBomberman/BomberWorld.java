6762a8c6a3250f9350b57924a3751b3c8e523fbf
Christian Sacher <Reyfin@gmx.de>
2017-11-24 12:45:52 +0100

    intial commit

---
+++ SuperBomberman/BomberWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BomberWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BomberWorld extends World
+{
+public int gridSize = 25;
+    /**
+     * Constructor for objects of class BomberWorld.
+     * 
+     */
+    public BomberWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
---
+++ SuperBomberman/InteractableActor.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class InteractableActor here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class InteractableActor extends Actor
+{
+    /**
+     * Act - do whatever the InteractableActor wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
